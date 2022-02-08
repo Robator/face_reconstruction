70651865068f4a4f7049c76156ae0224d201af38
Egor Matsuev <egor.matsuev@gmail.com>
2022-02-09 01:05:11 +0300

    Feature. Apply averaging to intersection of current and averaged image

--- merger.py
+++ merger.py
@@ -8,22 +8,52 @@ class Merger:
 		self.batch_size = batch
 		self.sum_image = np.zeros(shape[::-1], dtype=np.uint)
 
+	def find_intersection_by_ids(self, xy1: tuple):
+		"""
+		Find intersection between given array with x and y coordinates and
+		coordinates of average image which are not equal 255
+		:param xy1: x and y indices
+		:return: intersection mask
+		"""
+		xy2 = np.where(self.average_image < 255)
+
+		x_mask = np.zeros_like(xy1[0], dtype=bool)
+		y_mask = np.zeros_like(x_mask)
+		i = 0
+		for x1, x2 in zip(xy1[0], xy2[0]):
+			x_mask[i] = (x1 == x2)
+			i += 1
+		i = 0
+		for y1, y2 in zip(xy1[1], xy2[1]):
+			y_mask[i] = (y1 == y2)
+			i += 1
+		mask = (x_mask & y_mask)
+		return mask
+
 	def apply_average(self):
 		"""
-		Average pixels on image batch. Only non-white pixels are averaged
-		:return: None
+		Average pixels on intersection of current batch and average image.
+		Only non-white pixels are averaged
 		"""
 		if self.average_image is None:
 			self.average_image = self.sum_image
 
 		nonzero_values_ids = np.where(self.sum_image < 255)
-		# nonzero values of current image
+
+		# apply mask
+		intersection_mask = self.find_intersection_by_ids(nonzero_values_ids)
+		nonzero_values_ids_x, nonzero_values_ids_y = nonzero_values_ids
+		nonzero_values_ids_x = nonzero_values_ids_x[intersection_mask]
+		nonzero_values_ids_y = nonzero_values_ids_y[intersection_mask]
+		nonzero_values_ids = (nonzero_values_ids_x, nonzero_values_ids_y)
+
+		# take chosen values of current image
 		cur_nonzero = self.sum_image[nonzero_values_ids]
-		# the corresponding values on average image
+		# and values with corresponding indices on average image
 		av_nonzero = self.average_image[nonzero_values_ids]
 		averaged_pixels = 0.5 * (av_nonzero + cur_nonzero / self.batch_size)
 
-		# paste pixels on original image
+		# save average image in memory for next call
 		self.average_image[nonzero_values_ids] = averaged_pixels
 		self.average_image = self.average_image.astype(np.uint8)
 
--- tests/test_merger.py
+++ tests/test_merger.py
@@ -1,6 +1,7 @@
 import unittest
 from merger import Merger
 from PIL import Image
+import numpy as np
 
 
 class TestMerging(unittest.TestCase):
@@ -20,7 +21,46 @@ class TestMerging(unittest.TestCase):
 		av_img = merger.merge_to_one(img1)
 		self.assertEqual(av_img[0, 0], pixel_value)
 		self.assertEqual(av_img.shape, img_shape[::-1])
+
 		av_img = merger.merge_to_one(img2)
 		self.assertEqual(av_img[0, 0], pixel_value / 2)
+
 		av_img = merger.merge_to_one(img1)
 		self.assertEqual(av_img[0, 0], int((pixel_value / 2 + pixel_value) / 2))
+
+	def test_find_intersection_by_ids(self):
+		"""
+		Check output intersection mask is correct
+		"""
+		img_shape = (3, 1)  # cols, rows
+		batch_size = 1
+		merger = Merger(img_shape, batch_size)
+		pixel_value = 0
+
+		img1 = Image.new("L", img_shape, "white")
+		img1.putpixel((0, 0), pixel_value)
+		img1.putpixel((2, 0), pixel_value)
+		arr2 = (np.array([0, 0]), np.array([0, 2]))
+		av_img = merger.merge_to_one(img1)
+		mask = merger.find_intersection_by_ids(arr2)
+
+		self.assertTrue(np.all(mask == [True, True]))
+
+	def test_intersection(self):
+		"""
+		Only matching pixels should be averaged. In this case after merging black image into
+		white one only pixel at coordinate (0, 0) is changed
+		"""
+		img_shape = (116, 120)  # cols, rows
+		batch_size = 1
+		merger = Merger(img_shape, batch_size)
+		pixel_value = 150
+
+		img1 = Image.new("L", img_shape, "white")
+		img1.putpixel((0, 0), pixel_value)
+		img2 = Image.new("L", img_shape, "black")
+		av_img = merger.merge_to_one(img1)
+		av_img = merger.merge_to_one(img2)
+
+		self.assertEqual(av_img[0, 0], 75)
+		self.assertTrue(np.all(av_img[0:, 1:] == 255))
